b92607e6535beac946f4ea4881862181fe58256f
Nipun-Singh <116649693+Nipun-Singh@users.noreply.github.com>
2025-07-10 21:44:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def index():
 if __name__ == '__main__':
     if not os.path.exists('uploads'):
         os.makedirs('uploads')
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0', port=5000)
